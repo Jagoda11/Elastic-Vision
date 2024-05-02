d45cb76ad53f1fec7ce0f978e3fba596451ab403
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-05-02 23:40:24 +0200

    Refactor server test file to use Jest mocks and remove duplicate imports

---
+++ backend/src/index.test.js
@@ -0,0 +1,33 @@
+const http = require('http')
+
+jest.mock('http')
+jest.mock('@elastic/elasticsearch')
+jest.mock('dotenv')
+jest.mock('path')
+
+jest.mock('http', () => ({
+  createServer: jest.fn().mockReturnValue({ listen: jest.fn() }),
+}))
+jest.mock('@elastic/elasticsearch', () => ({
+  Client: jest.fn(),
+}))
+jest.mock('dotenv')
+jest.mock('path')
+
+describe('Server', () => {
+  let originalEnv
+
+  beforeEach(() => {
+    originalEnv = process.env
+    process.env = { ...originalEnv, ES_URL: 'http://localhost:9200' }
+  })
+
+  afterEach(() => {
+    process.env = originalEnv
+  })
+
+  it('starts the server correctly', () => {
+    require('./index')
+    expect(http.createServer).toHaveBeenCalled()
+  })
+})
